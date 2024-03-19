89dfabf67ef950d8a9ffb6870313061fd03a2696
Shohjahon03 <quvvatovshohjahon707@gmail.com>
2024-03-19 19:33:59 +0500

    ad

--- src/Pages/LoginP.tsx
+++ src/Pages/LoginP.tsx
@@ -9,7 +9,7 @@ const LoginP = ({setX}:any) => {
     }
   return (
     <div className="w-full h-dvh flex items-center justify-center">
-        <div>
+        <div className="flex flex-col">
         <input value={val.name} onChange={(e)=>setVal({name:e.target.value,email:val.email})} type="text" className=" outline-none bg-slate-200 text-black w-[400px] h-[56px] rounded-xl border-none " placeholder="# Username" />
             <input value={val.email} onChange={(e)=>setVal({name:val.name,email:e.target.value})} type="text" className=" outline-none bg-slate-200 text-black w-[400px] h-[56px] rounded-xl border-none " placeholder="# Useremali" />
             <button className="w-[400px] h-[56px] rounded-xl bg-amber-200" onClick={()=>func()}>Regster</button>
